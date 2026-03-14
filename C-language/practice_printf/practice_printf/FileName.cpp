d2207f218ac2bfccc9dbed7444ec61e232331a02
bomin seo <sbm8011@gmail.com>
2026-03-15 02:40:51 +0900

    feat: C언어 printf 실습 프로젝트 설정

---
+++ C-language/practice_printf/practice_printf/FileName.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    int dice1, dice2, dice3 = 0;
+
+    scanf("%d %d %d", &dice1, &dice2, &dice3);
+
+    if (dice1 == dice2 && dice1 == dice3) printf("%d\n", 10000 * dice1);
+    else (a == b || a == c || b == c)
+        elif
+}
